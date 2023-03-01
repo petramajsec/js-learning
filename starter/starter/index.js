56e5c280e31370024aa3a0d8a911445d4bc581a6
Petra Majsec <petra@spaceinch.com>
2023-03-01 13:47:37 +0100

    generation number from 0 to n

--- starter/starter/index.js
+++ starter/starter/index.js
@@ -1,3 +1,14 @@
-//1: postavi github repo 
+//1: postavi github repo DONE
 
 //2: napisi funkciju koja ce, nakon sto se klikne Button, u console logu svaki put prikazati neki randome broj
+console.log("Hello");
+
+
+/* var button = document.getElementsByTagName("doSomething");
+ console.log(button);*/
+
+ function numberGenarator(min, max){
+   // return number = Math.floor(Math.random()*1000);
+   return Math.floor(Math.random() * (max - min) + min);
+ }
+ console.log(numberGenarator(0,9999));
