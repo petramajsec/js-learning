e3a66169694a4b6121208dff385135d645eeca4b
Petra Majsec <petra@spaceinch.com>
2023-03-02 15:18:29 +0100

    solution 2, using callback

--- starter/starter/index.js
+++ starter/starter/index.js
@@ -4,15 +4,23 @@
 console.log("Hello");
 
 
-
-document.getElementById('doSomething').addEventListener("click", function() {  //addEventListener() method of the EventTarget interface sets up a function that will be called whenever the specified event is delivered to the target.
+// solution #1
+/*document.getElementById('doSomething').addEventListener("click", () => {  //addEventListener() method of the EventTarget interface sets up a function that will be called whenever the specified event is delivered to the target.
     function numberGenarator(min, max){
         return Math.floor(Math.random() * (max - min) + min);
         }
     
-    console.log(numberGenarator(0,9999)); });
-    
-    
-    
-    
+console.log(numberGenarator(0,9999)); });*/
 
+
+// solution #2
+let button = document.getElementById('doSomething');
+    button.onclick = () => { //button.onclick function is a callback function.
+        
+numberGenerator(0, 9999);
+    };
+
+var numberGenerator = (min, max) => {
+    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        console.log(randomNumber);
+};
